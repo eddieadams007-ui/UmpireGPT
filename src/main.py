68d39c52760282d83f5751f27f7aa2a0da6073a5
eddieadams007-ui <eddie.adams007@gmail.com>
2025-10-06 13:07:13 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
-from .rag import answer_query
+from .config import OPENAI_API_KEY, USE_OPENAI, KB_PATH
+from .retriever import retrieve_answer
 
 app = Flask(__name__)
 
@@ -12,7 +13,7 @@ def answer():
     data = request.get_json()
     query = data.get('query', '')
     k = data.get('k', 5)
-    response = answer_query(query, k)
+    response = retrieve_answer(query, k, KB_PATH, OPENAI_API_KEY, USE_OPENAI)
     return jsonify(response)
 
 if __name__ == '__main__':
