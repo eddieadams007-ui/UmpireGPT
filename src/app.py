97efab8bdbb06224834e6ae086e3cfe2be274078
Eddie Adams <eddieadams007-ui@users.noreply.github.com>
2025-10-07 21:10:48 +0000

    Add separate Streamlit service for Cloud Run

--- src/app.py
+++ src/app.py
@@ -1,16 +1,15 @@
 import streamlit as st
-   import requests
+import requests
 
-   st.title("UmpireGPT: Little League Rulebook Assistant")
-   st.write("Ask questions about Little League rules and get answers based on the Official Little League Rulebook.")
-   question = st.text_input("Enter your question:")
-   if st.button("Submit"):
-       if question:
-           # Use localhost for containerized FastAPI backend
-           response = requests.get(f"http://localhost:8000/query?question={question}")
-           if response.status_code == 200:
-               result = response.json()
-               st.write(f"**Question:** {result['question']}")
-               st.write(f"**Answer:** {result['answer']}")
-           else:
-               st.error(f"Error fetching response: {response.status_code}")
+st.title("UmpireGPT: Little League Rulebook Assistant")
+st.write("Ask questions about Little League rules and get answers based on the Official Little League Rulebook.")
+question = st.text_input("Enter your question:")
+if st.button("Submit"):
+    if question:
+        response = requests.get(f"https://umpiregpt-v1-qovl4xndza-uc.a.run.app/query?question={question}")
+        if response.status_code == 200:
+            result = response.json()
+            st.write(f"**Question:** {result['question']}")
+            st.write(f"**Answer:** {result['answer']}")
+        else:
+            st.error(f"Error fetching response: {response.status_code}")
